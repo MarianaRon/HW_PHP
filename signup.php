457074dabc17187b4d4298c7d841c7d6887d9cf4
MarianaRon <29051987ron@gmail.com>
2024-02-18 19:23:57 +0200

    hw_3

--- signup.php
+++ signup.php
@@ -1,143 +1,53 @@
 
 
-<div clas="container mt-4">
+<div class="container my-4">
     <h1>Реєстрація</h1>
-    <form action="post_test.php" method="post">
-        <input type="text" class="from-control" name="email"
-        id="email" placeholder="Введіть електронну пошту">
-        
-        <input type="text" class="from-control" name="name"
-        id="name" placeholder="Введіть ім'я">
-        
-        <input type="text" class="from-control" name="password"
-        id="password" placeholder="Введіть пароль">
-        
-        <input type="text" class="from-control" name="cpassword"
-        id="cpassword" placeholder="Введіть пароль повторно">
-        
-        <label for="avatar">ВИБЕРІТЬ СОБІ АВАТАРКУ</label> 
-        <input type="file"  class="form-control"
-         id="avatar" name="avatar" >
-        <br>
-        <br>
-         <button class="btn btn-success" type="submit">РЕЄСТРАЦІЯ</button>
-    </form>
-
-</div>
-
-<!--
-
-<div class="container my-4 ">
-    
-    <h1 class="text-center">Реєстрація</h1> 
-
-<form action="signup.php" method="post">
-    <div class="form-group"> 
-            <label for="email">Email</label> 
-        <input type="text" class="form-control" id="email"
-            name="email" aria-describedby="emailHelp">    
-        </div>
-    
-
-        <div class="form-group"> 
-            <label for="username">Username</label> 
-        <input type="text" class="form-control" id="username"
-            name="username" aria-describedby="emailHelp">    
-        </div>
-    
-        <div class="form-group"> 
-            <label for="password">Password</label> 
-            <input type="password" class="form-control"
-            id="password" name="password"> 
-        </div>
-    
-        <div class="form-group"> 
-            <label for="cpassword">Confirm Password</label> 
-            <input type="password" class="form-control"
-                id="cpassword" name="cpassword">
-    
-        </div> 
-        <div class="form-group"> 
-            <label for="avatar">Avatar</label> 
-            <input type="file"  class="form-control"
-                id="avatar" name="avatar">
-    
-        </div>   
-        <div class ="row">
-        <button type="submit" class="btn btn-primary">Реєстрація</button>
-        </div> 
-    </form> 
-</div>
--->
-   <!--
-<form >
-    <div class="row">
-        <div class="col">
-            <div class="input-group mb-3">
-                <span class="input-group-text" 
-                      id="login-addon"><i class="bi bi-person"></i></span>
-                <input type="text" class="form-control" 
-                       placeholder="Логін" aria-label="Логін"
-                       name="signup-login"
-                       aria-describedby="login-addon">
-            </div>
-        </div>
-        <div class="col">
-            <div class="input-group mb-3">
-                <span class="input-group-text"
-                      id="name-addon"><i class="bi bi-person-vcard"></i></span>
-                <input type="text" class="form-control"
-                       placeholder="ПІБ" aria-label="ПІБ"
-                       name="signup-name"
-                       aria-describedby="name-addon">
-            </div>
-        </div>
+    <form class="col s12" method="post">
+    <div class="input-field col s6">
+         <input  id="icon_email" type="tel"  name="user-email">
+         <i class="material-icons prefix">mail</i>
+         <label for="icon_email">Введіть E-mail</label>
+         <span class="helper-text"
+               data-error="Це необхідне поле"
+               data-success="Правильно"></span>
     </div>
-    <div class="row">
-        <div class="col">
-            <div class="input-group mb-3">
-                <span class="input-group-text"
-                      id="email-addon"><i class="bi bi-envelope-at"></i></span>
-                <input type="text" class="form-control"
-                       placeholder="Е-пошта" aria-label="Е-пошта"
-                       name="signup-email"
-                       aria-describedby="email-addon">
-            </div>
-        </div>
-        <div class="col">
-            <div class="input-group mb-3">
-                <label class="input-group-text"
-                       for="avatar-input"><i class="bi bi-person-circle"></i></label>
-                <input type="file" class="form-control"
-                       placeholder="Аватарка" aria-label="Аватарка"
-                       name="signup-avatar"
-                       id="avatar-input">
-            </div>
-        </div>
+        
+    <div class="input-field col s6">
+        <input id="icon_prefix" type="text" name="user-name" class="" >
+        <i class="material-icons prefix">account_circle</i>
+        <label for="icon_prefix">Введіть прізвище, ім'я, по-батькові</label>
+        <span class="helper-text"data-error=""
+              data-success="Правильно"></span>
     </div>
-    <div class="row">
-        <div class="col">
-            <div class="input-group mb-3">
-                <span class="input-group-text"
-                      id="password-addon"><i class="bi bi-lock"></i></span>
-                <input type="password" class="form-control"
-                       placeholder="Пароль" aria-label="Пароль"
-                       name="signup-password"
-                       aria-describedby="password-addon">
-            </div>
-        </div>
-        <div class="col">
-            <div class="input-group mb-3">
-                <span class="input-group-text"
-                      id="repeat-addon"><i class="bi bi-unlock"></i></span>
-                <input type="password" class="form-control"
-                       placeholder="Повторіть пароль" aria-label="Повторіть пароль"
-                       name="signup-repeat"
-                       aria-describedby="repeat-addon">
-            </div>
-        </div>
+        
+    <div class="input-field col s6">
+        <input  id="icon_password" type="password" name="user-password" >
+        <i class="material-icons prefix">lock</i>
+        <label for="icon_password">Введіть пароль</label>
+        <span class="helper-text"
+              data-error="Це необхідне поле"
+              data-success="Припустимо"></span>
     </div>
-
-    <div class ="row">
-        <button class="btn btn-success" type="submit">Реєстрація</button>
-    </div>--> 
+        
+    <div class="input-field col s6">
+        <input id="icon_repeat" type="password"  name="user-repeat" inputmode="tel">
+        <i class="material-icons prefix">key</i>
+        <label for="icon_repeat">Повторіть введений пароль</label>
+        <span class="helper-text"
+              data-error="Це необхідне поле"
+              data-success="Правильно"></span>
+    </div>
+        
+    <div class="file-field input-field col s6">
+        <div class="btn pink darken-2">
+            <i class="material-icons">photo</i>
+            <input type="file">
+        </div>
+        <div class="file-path-wrapper">
+            <input class="file-path validate" type="text" placeholder="Аватарка">
+        </div>
+    </div><br>
+    <div class="input-field col s6">
+         <button class="btn btn-success" type="submit"><i class="material-icons left">task_alt</i>РЕЄСТРАЦІЯ</button>
+         </div>
+ </form></div>
